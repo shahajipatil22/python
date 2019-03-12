725b925e3590f16ba9e372c287d972d2766cca98
shahajipatil22 <33981606+shahajipatil22@users.noreply.github.com>
2019-03-12 15:14:01 +0530

    Update syslogsample.py

--- unp/3/syslogsample.py
+++ unp/3/syslogsample.py
@@ -38,3 +38,33 @@ syslog.syslog("I am a terimiting.")
 
 
 
+#s = raw_input()
+#i = 0
+#for i in s:
+#    print i
+#raw_input()
+
+#扩展成在字符串中把特定字符替换成规定字符
+import re
+def printReplace(srcStr,word):
+    for letter in srcStr:
+        if letter == word:
+            srcStr = srcStr.replace(letter,'!') #字符串不可更改
+    return srcStr
+
+def printReplace_re(srcStr,word):
+    srcStr=re.sub(word,'!',srcStr)
+    return srcStr
+
+def main():    
+    srcStr = raw_input(u"请输入一个字符串：")
+    destStr=printReplace_re(srcStr,'i')
+    print destStr
+
+
+if __name__ == "__main__":
+    main()
+            
+
+    © 2019 GitHub, Inc.
+    Terms
